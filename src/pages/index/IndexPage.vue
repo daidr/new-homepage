55bc9e28bf71d8fa3841bb98963fdecba6a3f0ff
daidr <daidr@daidr.me>
2022-12-25 17:42:57 +0800

    feat: home page

---
+++ src/pages/index/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="index-page-wrapper">
+        <div class="main-menu-wrapper">
+            <div class="menu-item">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+#index-page-wrapper {
+    @apply "bg-white";
+    @apply "w-[350px] sm:w-[400px] h-[500px]";
+    @apply "rounded-4xl";
+    @apply "fixed top-1/2 left-1/2";
+    @apply "transform-gpu -translate-x-1/2 -translate-y-1/2";
+}
+</style>
