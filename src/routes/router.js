22c3d1243ac5367a80f48e327f6ca1717cf933e6
daidr <daidr@daidr.me>
2022-12-26 16:28:38 +0800

    chore: remove unused scripts

--- src/routes/router.js
+++ src/routes/router.js
@@ -19,29 +19,6 @@ const router = VueRouter.createRouter({
   routes,
 });
 
-const togglePageLoadingState = (() => {
-  let TIMER = null;
-  return (state, from, to) => {
-    if (state == 1) {
-      TIMER = setTimeout(() => {
-        // 一百毫秒后，如果页面还没有加载完毕，就显示加载动画
-        // 此时前页面缩小成加载动画的大小
-        pageAttrStore.setPageHasLoadingState(true);
-        pageAttrStore.setPageLoadingState(true);
-      }, 100);
-    } else if (state == 0) {
-      // 清除页面加载计时器
-      clearTimeout(TIMER);
-      if (pageAttrStore.pageHasLoadingState.value) {
-        // 有加载动画
-        pageAttrStore.setPageLoadingState(false);
-      } else {
-        pageAttrStore.setPageHasLoadingState(false);
-      }
-    }
-  };
-})();
-
 router.beforeEach((to, from, next) => {
   // togglePageLoadingState(1, from, to);
   next();
--- src/stores/pageAttrStore.js
+++ src/stores/pageAttrStore.js
@@ -2,30 +2,5 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const usePageAttrStore = defineStore("page-attr", () => {
-  const pageLoadingState = ref(true);
-  const setPageLoadingState = (state) => {
-    pageLoadingState.value = state;
-  };
-
-  const pageHasLoadingState = ref(false);
-  const setPageHasLoadingState = (state) => {
-    pageHasLoadingState.value = state;
-  };
-
-  const routerWrapperSelector = ref(["", ""]);
-  const setRouterWrapperSelector = (from, to) => {
-    routerWrapperSelector.value = [
-      from.meta.calcContainer,
-      to.meta.calcContainer,
-    ];
-  };
-
-  return {
-    pageLoadingState,
-    setPageLoadingState,
-    pageHasLoadingState,
-    setPageHasLoadingState,
-    routerWrapperSelector,
-    setRouterWrapperSelector,
-  };
+  return {};
 });
