c4d531caf98e66f6bf51f0125715a5b510931030
daidr <daidr@daidr.me>
2022-12-30 16:20:49 +0800

    chore: add tools to make debug more easier

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,9 @@ import Icons from "unplugin-icons/vite";
 import IconsResolver from "unplugin-icons/resolver";
 import Components from "unplugin-vue-components/vite";
 
+import Inspect from "vite-plugin-inspect";
+import Inspector from "vite-plugin-vue-inspector";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -27,6 +30,10 @@ export default defineConfig({
       }),
       apply: "build",
     },
+    Inspect(),
+    Inspector({
+      toggleButtonPos: "bottom-left"
+    }),
   ],
   resolve: {
     alias: {
