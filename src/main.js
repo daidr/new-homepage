a158309284bd7f280aa5ed25dfd0f36e8fb996f6
daidr <daidr@daidr.me>
2022-12-30 16:38:46 +0800

    feat: remove pinia

--- src/main.js
+++ src/main.js
@@ -8,11 +8,6 @@ import "@/assets/css/global.scss";
 
 const root = createApp(App);
 
-// pinia
-import { createPinia } from "pinia";
-const pinia = createPinia();
-root.use(pinia);
-
 // 路由
 import { getRouter } from "@/routes/router";
 const router = getRouter();
--- src/stores/pageAttrStore.js
+++
@@ -1,6 +0,0 @@
-import { defineStore } from "pinia";
-import { ref } from "vue";
-
-export const usePageAttrStore = defineStore("page-attr", () => {
-  return {};
-});
